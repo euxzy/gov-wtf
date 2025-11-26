53e9f8f1194b3a0ff59350551b7d0341037a2d47
euxzy <mhd.rijal203@gmail.com>
2025-11-26 21:57:23 +0700

    feat: integrate pagination and search

--- app/components/shared/header.tsx
+++ app/components/shared/header.tsx
@@ -1,16 +1,32 @@
 import { SearchIcon } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router'
 import { InputGroup, InputGroupAddon, InputGroupInput } from '~/components/ui/input-group'
+import { useDebounce } from '~/hooks/use-debounce'
 
 export const Header = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [search, setSearch] = useState(searchParams.get('q') || '')
+  const searchDebounce = useDebounce(search, 300)
+
+  useEffect(() => {
+    if (!searchDebounce) return setSearchParams({ page: '1' })
+    setSearchParams({ page: '1', q: searchDebounce })
+  }, [searchDebounce])
+
   return (
     <header className="border-b sticky top-0 z-100 bg-background">
       <div className="py-6 max-w-7xl mx-auto flex items-center justify-between px-5 md:px-8 lg:px-0">
         <h1 className="text-2xl font-semibold [&>span]:text-primary">
           Gov<span>WTF</span>
         </h1>
-        <div className="w-72 hidden md:block">
+        <div className="md:w-72">
           <InputGroup>
-            <InputGroupInput placeholder="Search..." />
+            <InputGroupInput
+              placeholder="Search..."
+              defaultValue={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
             <InputGroupAddon>
               <SearchIcon />
             </InputGroupAddon>
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,15 @@
-import { SearchIcon } from 'lucide-react'
+import { like, or } from 'drizzle-orm'
+import { useSearchParams } from 'react-router'
 import { PersonCard } from '~/components/shared/card/person'
-import { InputGroup, InputGroupAddon, InputGroupInput } from '~/components/ui/input-group'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '~/components/ui/pagination'
 import { db } from '~/db'
 import { functionary } from '~/db/schema'
 import type { Route } from './+types'
@@ -9,17 +18,40 @@ export function meta(_args: Route.MetaArgs) {
   return [{ title: 'Gov WTF' }, { name: 'description', content: 'The worst government in the world!' }]
 }
 
-export const loader = async () => {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  const url = new URL(request.url)
+  const searchParams = url.searchParams
+  const page = Number(searchParams.get('page') || 1)
+  const q = searchParams.get('q') || ''
+
   try {
-    const functionaries = (await db.select().from(functionary).limit(10)) || []
-    return { functionaries }
+    const [functionaries, functionaryCount] = await Promise.all([
+      db
+        .select()
+        .from(functionary)
+        .limit(8)
+        .offset((page - 1) * 8)
+        .where(or(like(functionary.name, `%${q}%`), like(functionary.position, `%${q}%`))),
+      db.$count(functionary),
+    ])
+
+    return { functionaries, functionaryCount, page, totalPage: Math.ceil(functionaryCount / 8) }
   } catch (err) {
     console.error(err)
-    return { functionaries: [] }
+    return { functionaries: [], functionaryCount: 0, page, totalPage: 1 }
   }
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
+  const [serachParams, setSearchParams] = useSearchParams()
+
+  const onChangePage = (page: number) => {
+    if (page < 1 || page > loaderData.totalPage) return
+
+    serachParams.set('page', String(page))
+    setSearchParams(serachParams)
+  }
+
   return (
     <section>
       <div className="max-w-7xl mx-auto py-8 px-5 md:px-8 lg:px-0">
@@ -30,19 +62,54 @@ export default function Home({ loaderData }: Route.ComponentProps) {
           <p className="text-muted-foreground lg:text-lg">Berikan penilaianmu terhadap kinerja pejabat negeri</p>
         </div>
 
-        <div className="mb-5 md:hidden">
-          <InputGroup>
-            <InputGroupInput placeholder="Search..." />
-            <InputGroupAddon>
-              <SearchIcon />
-            </InputGroupAddon>
-          </InputGroup>
-        </div>
-
         <div className="grid gap-3 grid-cols-2 md:grid-cols-3 md:gap-4 lg:grid-cols-4">
           {loaderData.functionaries.map((functionary, i) => (
             <PersonCard key={`${i + 0}`} {...functionary} />
           ))}
+        </div>
+
+        <div className="py-12">
+          <Pagination className="justify-end select-none">
+            <PaginationContent>
+              <PaginationItem onClick={() => onChangePage(loaderData.page - 1)}>
+                <PaginationPrevious className="cursor-pointer" />
+              </PaginationItem>
+
+              <PaginationItem onClick={() => onChangePage(1)}>
+                <PaginationLink isActive={loaderData.page === 1} className="cursor-pointer">
+                  1
+                </PaginationLink>
+              </PaginationItem>
+              {loaderData.page > 4 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+              {Array.from({ length: 3 }, (_, idx) => loaderData.page + idx)
+                .filter((page) => page > 1 && page < loaderData.totalPage)
+                .map((page) => (
+                  <PaginationItem key={`page-${page}`} onClick={() => onChangePage(page)}>
+                    <PaginationLink isActive={loaderData.page === page} className="cursor-pointer">
+                      {page}
+                    </PaginationLink>
+                  </PaginationItem>
+                ))}
+              {loaderData.page < loaderData.totalPage - 4 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+              <PaginationItem onClick={() => onChangePage(loaderData.totalPage)}>
+                <PaginationLink isActive={loaderData.page === loaderData.totalPage} className="cursor-pointer">
+                  {loaderData.totalPage}
+                </PaginationLink>
+              </PaginationItem>
+
+              <PaginationItem onClick={() => onChangePage(loaderData.page + 1)}>
+                <PaginationNext className="cursor-pointer" />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </div>
     </section>
